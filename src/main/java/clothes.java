b2082833864b0c4762d540e7fb101e0129a95214
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-09-02 15:31:01 +0200

    Start: create class clothes

---
+++ src/main/java/clothes.java
@@ -0,0 +1,48 @@
+import java.util.Objects;
+
+public class clothes {
+  private final String color; //ToDo: schützen das die Farbe später nicht verändert werden kann
+  private final String size;
+  private final String seasonUsage;
+  private final String type;
+  private final String material;
+  private Boolean clean;
+  private String washingNotes;
+  clothes(String color, String size, String seasonUsage, String type, String material, Boolean clean) {
+    this.color = color;
+    this.size = size;
+    this.seasonUsage = seasonUsage;
+    this.type = type;
+    this.material = material;
+    this.clean = clean;
+  }
+
+  public String getType() {
+    return type;
+  }
+  public Boolean getClean() {
+    return clean;
+  }
+  public void setClean(Boolean clean) {
+    this.clean = clean;
+  }
+  public String getWashingNotes() {
+    return Objects.requireNonNullElse(washingNotes, "");//ToDo: return Exeption
+  }
+  public void setWashingNotes(String washingNotes) {
+    this.washingNotes = washingNotes;
+  }
+
+  public String getColor() {
+    return color;
+  }
+  public String getSize() {
+    return size;
+  }
+  public String getSeasonUsage() {
+    return seasonUsage;
+  }
+  public String getMaterial() {
+    return material;
+  }
+}
