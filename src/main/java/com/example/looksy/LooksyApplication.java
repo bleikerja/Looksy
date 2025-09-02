ce5fec459e0ad45ebe0d8fcf7f865b9bd3cb264f
k-llemke <s_lemke24@stud.hwr-berlin.de>
2025-09-02 15:11:13 +0200

    Start: set up

---
+++ src/main/java/com/example/looksy/LooksyApplication.java
@@ -0,0 +1,13 @@
+package com.example.looksy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LooksyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LooksyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/looksy/LooksyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.looksy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooksyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
